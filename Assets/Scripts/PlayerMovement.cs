6a95bb46743017853336715e6485ccc9c0c6fafb
CoolingBroski <camarenapliego@gmail.com>
2019-09-22 17:48:33 -0500

    Added player immortality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,31 +13,46 @@ public class PlayerMovement : MonoBehaviour {
 	float nextFire = 0.0f;
 	public int lives;
 	public int score;
+    private bool invincible;
+    private float invtime;
 	
 	void Start () {
 		lives = 3;
 		score = 0;
-	}
-	
-	void Update () {
-		velocityX = 0f;
-		if (Input.GetKey ("right")) {
-			velocityX = speed;
+        invincible = false;
+    }
+    void Update()
+    {
+        if (invincible == true)
+        {
+            StartCoroutine(Immortal());
+            if (Time.time > invtime + 3f)
+            {
+                invincible = false;
+            }
+        }
+        velocityX = 0f;
+        if (Input.GetKey("right"))
+        {
+            velocityX = speed;
 
-		}
-		if (Input.GetKey ("left")) {
-			velocityX = -speed;
-		}
-		
-		if(GetComponent<Rigidbody2D>()){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX,0);
-		}
+        }
+        if (Input.GetKey("left"))
+        {
+            velocityX = -speed;
+        }
 
-		if(Input.GetButtonDown("Fire1") && Time.time > nextFire){
-			nextFire = Time.time + fireRate;
-			fire();
-		}
-	}
+        if (GetComponent<Rigidbody2D>())
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, 0);
+        }
+
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            fire();
+        }
+    }
 
 	void fire(){
 		balitaPosVector = transform.position;
@@ -45,16 +60,34 @@ public class PlayerMovement : MonoBehaviour {
 		clone = (GameObject)Instantiate (balita, balitaPosVector, Quaternion.identity);
 	}
 
+    IEnumerator Immortal() // Hacer que parpadee
+    {
+        while (invincible == true)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            yield return new WaitForSeconds(.1f);
+            GetComponent<SpriteRenderer>().enabled = true;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
 	void OnCollisionEnter2D(Collision2D collider){
-		if(collider.gameObject.tag == "Enemy"){
-			lives--;
-			Destroy(collider.gameObject);
-			if(lives == 0){
-				print("Your score is:" + score);
-				Destroy(gameObject);
-			}
-			//GetComponent<Collider>().enabled = false;           
-		}
+        if (collider.gameObject.tag == "Enemy")
+        {
+            if (invincible == false)
+            {
+                lives--;
+                invincible = true;
+                invtime = Time.time;
+            }
+            Destroy(collider.gameObject);
+            if (lives == 0)
+            {
+                print("Your score is:" + score);
+                Destroy(gameObject);
+            }
+            //GetComponent<Collider>().enabled = false;
+        }
 	}
 }
 
