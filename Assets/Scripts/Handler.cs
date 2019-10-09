3bc3bc98418a0b3915d20a4682ca92f2133be6e7
ktsb3 <38196743+ktsb3@users.noreply.github.com>
2019-10-09 00:00:55 -0500

    Buttons
    Added Buttons

---
+++ Assets/Scripts/Handler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Handler : MonoBehaviour{ 
+    public string scene;
+    private bool loadlock;
+    public int stocks;
+    public GameObject stock;
+    public Button button;
+    public Button button2;
+    
+    void Start(){
+        button = GameObject.FindGameObjectWithTag("button").GetComponent<Button>();
+        button.gameObject.SetActive(false);
+        button2 = GameObject.FindGameObjectWithTag("button2").GetComponent<Button>();
+        button2.gameObject.SetActive(false);
+    }
+
+    void Update(){
+        lives playerscript = stock.GetComponent<lives>();
+        stocks = playerscript.lives;
+        if(stocks == 0){
+            button.gameObject.SetActive(true);
+            button2.gameObject.SetActive(true);
+        }
+    }
+
+    public void Restart(string scene){
+        Application.LoadLevel(scene);
+    }
+
+    public void Menu(string scene){
+        Application.LoadLevel(scene);
+    }
+}
---
+++ Assets/Scripts/lives.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour{
+    public GameObject player;
+    public int lives;
+    
+    void Start(){
+        
+    }
+
+    void Update(){
+        PlayerMovement playerscript = player.GetComponent<PlayerMovement>();
+        lives = playerscript.lives;
+    }
+}
